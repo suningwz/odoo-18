1f99c935699212768dc5fb37c689083caddbbfdf
sergiosaidman <83665718+sergiosaidman@users.noreply.github.com>
2021-05-21 16:58:58 -0500

    Delete payment_conekta_oxoo directory

--- payment_conekta_oxoo/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import controllers
-from . import models
-from odoo.addons.payment_conekta_oxoo.models.payment_acquirer import create_missing_journal_for_conekta_acquirers
